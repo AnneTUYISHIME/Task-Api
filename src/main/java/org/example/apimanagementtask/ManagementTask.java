5dbd12b4836e3b3e01b54158daeae973388a9980
Tuyishime Anne <atuyishime28@gmail.com>
2025-08-14 22:23:21 +0200

    initial commit

---
+++ src/main/java/org/example/apimanagementtask/ManagementTask.java
@@ -0,0 +1,13 @@
+package org.example.apimanagementtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementTask {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementTask.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/apimanagementtask/task/Task.java
@@ -0,0 +1,25 @@
+package org.example.apimanagementtask.task;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private String status;
+}
